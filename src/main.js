966ed25c61975889bb095a609adc45a63887145d
Emanuele discala <manueldiscala@gmail.com>
2023-05-25 15:01:50 +0200

    first commit

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import './generic.scss'
+import App from './App.vue'
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import "axios"
+createApp(App).mount('#app')
